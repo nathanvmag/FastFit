b374aa7c8789b7c25db296e8c7cb509ce6ec42d1
Nathan Vieira <nathanvmag@gmail.com>
2017-08-31 12:24:25 -0300

    Slider

---
+++ Assets/MenuMover.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMover : MonoBehaviour {
+    [SerializeField]
+    GameObject[] Scenes;
+    int goTO;
+	// Use this for initialization
+	void Start () {
+        goTO = 2;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.Lerp(new Vector3(transform.position.x, transform.position.y, -10), new Vector3(Scenes[goTO].transform.position.x, Scenes[goTO].transform.position.y,-10), 5 * Time.deltaTime);
+	}
+    public void layoutControler(int where)
+    {
+        goTO = where;
+        Debug.Log("CLique");
+    }
+}
