2aa089f2ef853bb3e5d536ecf778c2d14a797f86
Nathan Vieira <nathanvmag@gmail.com>
2017-10-11 07:01:33 -0300

    calendar

---
+++ Assets/ControlCalendar.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ControlCalendar : MonoBehaviour {
+    [SerializeField]
+    GameObject horarioprefab;
+	// Use this for initialization
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void clickHorario(GameObject gb)
+    {
+        GameObject father = GameObject.FindGameObjectWithTag("calendarContent");
+        if (!gb.GetComponent<dayweek>().isOpen)
+        {
+            GameObject horario = Instantiate(horarioprefab, father.transform.position, Quaternion.identity, father.transform) as GameObject;
+            father.GetComponent<RectTransform>().sizeDelta += new Vector2(0, 276);
+            horario.transform.SetSiblingIndex(gb.transform.GetSiblingIndex() + 1);
+            int child = 0;
+           foreach(Transform t in horario.transform)
+            {
+               t.GetComponent<Toggle>().isOn = gb.GetComponent<dayweek>().values[child];
+                child++;
+            }
+
+        }
+        else
+        {
+            Destroy(father.transform.GetChild(gb.transform.GetSiblingIndex() + 1).gameObject);
+            father.GetComponent<RectTransform>().sizeDelta += new Vector2(0,- 276);
+        }
+        gb.GetComponent<dayweek>().isOpen = !gb.GetComponent<dayweek>().isOpen;
+    }
+}
---
+++ Assets/Scripts/dayweek.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class dayweek : MonoBehaviour {
+    public bool isOpen;
+    public bool[] values;
+	// Use this for initialization
+	void Start () {
+        values = new bool[24];
+        for (int i = 0; i < values.Length; i++) values[i] = false;
+        isOpen = false;
+        GameObject father = GameObject.FindGameObjectWithTag("calendarContent");
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerClick;
+        entry.callback.AddListener((eventData) => { father.GetComponent<ControlCalendar>().clickHorario(gameObject); });
+
+
+        gameObject.GetComponent<EventTrigger>().triggers.Add(entry);
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (isOpen)
+        {
+            int child = 0;
+            foreach( Transform t in transform.parent.GetChild(transform.GetSiblingIndex()+1))
+            {
+              values[child]= t.GetComponent<Toggle>().isOn;
+                child++;
+            }
+        }
+	}
+}
