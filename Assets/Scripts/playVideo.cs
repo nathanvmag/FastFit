d0cab6d4b251d885b57cc5effa39c410ded4bda0
Henrique Filho <hbaf71@gmail.com>
2017-11-07 11:29:29 -0200

    Começando a colocar videos (falta quicktime)

---
+++ Assets/Scripts/playVideo.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(AudioSource))]
+public class playVideo : MonoBehaviour {
+
+    public MovieTexture movie;
+    private AudioSource audio;
+
+	void Start () {
+        GetComponent<RawImage>().texture = movie as MovieTexture;
+        audio = GetComponent<AudioSource>();
+        audio.clip = movie.audioClip;
+        movie.Play();
+        audio.Play();
+	}
+
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.Space) && movie.isPlaying)
+        {
+            movie.Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && !movie.isPlaying)
+        {
+            movie.Play();
+        }
+    }
+}
